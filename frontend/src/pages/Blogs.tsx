8a821f3b54a50c868b995cb6e5c16ed6a7a5a19d
Swaraj Roy <contactswarajroy@gmail.com>
2024-08-12 10:51:37 +0100

    fixed a breaking issue whilst loading the blogs page

--- frontend/src/pages/Blogs.tsx
+++ frontend/src/pages/Blogs.tsx
@@ -1,41 +1,51 @@
-import { BlogCard } from "../components/BlogCard"
-import { Appbar } from "../components/Appbar"
+import { BlogCard } from "../components/BlogCard";
+import { Appbar } from "../components/Appbar";
 import { BlogSkeleton } from "../components/BlogSkeleton";
 import { useBlogs } from "../hooks/index";
 
+export const Blogs = () => {
+  const { loading, blogs } = useBlogs();
 
-
-export const Blogs = ()=>{
-    const { loading, blogs} = useBlogs();
-
-    if (loading) {
-        return <div>
-            <Appbar /> 
-            <div  className="flex justify-center">
-                <div>
-                    <BlogSkeleton />
-                    <BlogSkeleton />
-                    <BlogSkeleton />
-                    <BlogSkeleton />
-                    <BlogSkeleton />
-                </div>
-            </div>
-        </div>
-    }
-
-    return <div>
+  if (loading) {
+    return (
+      <div>
         <Appbar />
-        <div  className="flex justify-center">
-            <div>
-                {blogs.map((blog: { id: number; author: { name: any; }; title: string; content: string; }) => <BlogCard
-                    id={blog.id}
-                    authorName={blog.author.name || "Anonymous"}
-                    title={blog.title}
-                    content={blog.content}
-                    PublishedDate={"2nd Feb 2024"}
-                />)}
-            </div>
+        <div className="flex justify-center">
+          <div>
+            <BlogSkeleton />
+            <BlogSkeleton />
+            <BlogSkeleton />
+            <BlogSkeleton />
+            <BlogSkeleton />
+          </div>
         </div>
-    </div>
+      </div>
+    );
+  }
 
-}
+  return (
+    <div>
+      <Appbar />
+      <div className="flex justify-center">
+        <div>
+          {blogs.map(
+            (blog: {
+              id: number;
+              author: { name: string | any };
+              title: string;
+              content: string;
+            }) => (
+              <BlogCard
+                id={blog.id}
+                authorName={blog.author.name || "Anonymous"}
+                title={blog.title}
+                content={blog.content}
+                publishedDate={"2nd Feb 2024"}
+              />
+            )
+          )}
+        </div>
+      </div>
+    </div>
+  );
+};
