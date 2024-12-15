91b9c6559d436dfb173a4d9ea1a21a172197d654
Jyotsna Kumar <atwaljyotsna@gmail.com>
2024-12-15 23:18:46 +0530

    changed middleware

--- backend/src/middleware/cors.ts
+++ backend/src/middleware/cors.ts
@@ -1,15 +1,23 @@
 // src/middlewares/cors.ts
-import { cors } from "hono/cors";
+import { MiddlewareHandler } from "hono";
 
-export const corsMiddleware = cors({
-  origin: (origin) => {
-    const allowedOrigins = [
-      "https://blog-jyotsna-kumars-projects.vercel.app",
-      "http://localhost:5173",
-    ];
-    return allowedOrigins.includes(origin) ? origin : "";
-  },
-  allowHeaders: ["Content-Type", "Authorization"],
-  allowMethods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-  credentials: true,
-});
+export const corsMiddleware: MiddlewareHandler = async (c, next) => {
+  const allowedOrigins = [
+    "http://localhost:5173",
+    "https://blog-git-master-jyotsna-kumars-projects.vercel.app",
+  ];
+  const origin = c.req.header("Origin");
+
+  if (origin && allowedOrigins.includes(origin)) {
+    c.header("Access-Control-Allow-Origin", origin);
+    c.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+    c.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+    c.header("Access-Control-Allow-Credentials", "true");
+  }
+
+  if (c.req.method === "OPTIONS") {
+    return c.text("", 204);
+  }
+
+  await next();
+};
